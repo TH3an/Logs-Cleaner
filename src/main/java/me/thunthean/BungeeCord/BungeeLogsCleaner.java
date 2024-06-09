ce37ebf0dbe4fead3d0eb33dbe18c971b8c2a9fc
thunthean <71697130+TH3an@users.noreply.github.com>
2024-06-09 16:37:37 +0700

    Adding Stuffs

---
+++ src/main/java/me/thunthean/BungeeCord/BungeeLogsCleaner.java
@@ -0,0 +1,15 @@
+package me.thunthean.BungeeCord;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeLogsCleaner extends Plugin {
+    @Override
+    public void onEnable() {
+        // Enable Logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Disable logic
+    }
+}
